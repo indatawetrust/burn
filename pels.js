6759bf732aa152df22041baf25ea18c3b42e7db1
Ahmet Simsek <iamaroott@gmail.com>
2018-04-28 16:34:15 +0300

    Update pels.js

--- pels.js
+++ pels.js
@@ -4,10 +4,24 @@ const pels = (function(){
     get: function(obj, prop) {
     	if (prop === '$') {
       	return handler.queue.pop();
+      } else if (prop === '$attrs') {
+      	const elem = handler.queue.pop();
+        
+        return new Proxy([...elem.attributes]
+                .map(({name, value}) => ({
+                  [name]: value
+                }))
+                .reduce((a,b) => ({...a,...b})), {
+                	set: (obj, key, value) => {
+                  	elem.setAttribute(key, value);
+                  }
+                });
       } else {
       	let elem = handler.queue.pop();
         
-        elem = elem ? elem.querySelector(prop) : document.querySelector(prop);
+        elem = elem
+        ? elem.querySelector(prop)
+        : document.querySelector(prop);
         	
         handler.queue.push(elem)
         
