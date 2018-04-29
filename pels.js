1a947f6c4fcebcc3edff99427fc42b6fbef08fa1
Ahmet Simsek <iamaroott@gmail.com>
2018-04-29 12:59:55 +0300

    Update pels.js

--- pels.js
+++ pels.js
@@ -1,8 +1,52 @@
 const pels = (function() {
+  const switchCase = (elem, key, value) => {
+    switch (key) {
+      case '$text':
+        elem.innerText = value;
+        break;
+      case '$textReplace':
+        elem.innerText = elem.innerText.replace(...value);
+        break;
+      case '$html':
+        elem.innerHTML = value;
+        break;
+      case '$on':
+        elem.addEventListener(...value);
+        break;
+      case '$append':
+        elem.appendChild(value);
+        break;
+      case '$render':
+        const render = () => elem.innerHTML = value(handler.data);
+        handler.renders.push(render);
+        render();
+        break;
+    }
+  };
+
+  const $attrs = elem => {
+    const attrs = [...elem.attributes].map(({name, value}) => ({
+      [name]: value,
+    }));
+
+    return new Proxy(
+      attrs.length ? attrs.reduce((a, b) => ({...a, ...b})) : {},
+      {
+        set: (obj, key, value) => {
+          elem.setAttribute(key, value);
+        },
+        get: (obj, key) => {
+          return elem.getAttribute(key);
+        },
+      },
+    );
+  };
+
   const handler = {
     data: {},
     queue: [],
     renders: [],
+    elements: {},
     get: function(obj, prop) {
       if (prop === '$') {
         return handler.queue.pop();
@@ -11,7 +55,7 @@ const pels = (function() {
           {},
           {
             get: (obj, key) => {
-              return (text, attrs) => {
+              return (text, attrs, name) => {
                 const elem = document.createElement(key);
 
                 elem.innerText = text;
@@ -22,11 +66,41 @@ const pels = (function() {
                   elem.setAttribute(key, value);
                 }
 
+                if (name) {
+                  handler.elements[name] = elem;
+                }
+
                 return elem;
               };
             },
           },
         );
+      } else if (prop === '$set') {
+        const elem = handler.queue.pop();
+
+        return key => {
+          handler.elements[key] = elem;
+        };
+      } else if (prop === '$get') {
+        return key => {
+          const elem = handler.elements[key];
+
+          return new Proxy(
+            {},
+            {
+              get: (obj, prop) => {
+                if (prop == '$') {
+                  return elem;
+                } else if (prop == '$attrs') {
+                  return $attrs(elem);
+                }
+              },
+              set: (obj, key, value) => {
+                switchCase(elem, key, value);
+              },
+            },
+          );
+        };
       } else if (prop === '$data') {
         return new Proxy(handler.data, {
           get: (obj, key) => {
@@ -43,18 +117,7 @@ const pels = (function() {
       } else if (prop === '$attrs') {
         const elem = handler.queue.pop();
 
-        const attrs = [...elem.attributes].map(({name, value}) => ({
-          [name]: value,
-        }));
-
-        return new Proxy(
-          attrs.length ? attrs.reduce((a, b) => ({...a, ...b})) : {},
-          {
-            set: (obj, key, value) => {
-              elem.setAttribute(key, value);
-            },
-          },
-        );
+        $attrs(elem);
       } else {
         let elem = handler.queue.pop();
 
@@ -68,28 +131,7 @@ const pels = (function() {
     set: (obj, key, value) => {
       const elem = handler.queue.pop();
 
-      switch (key) {
-        case '$text':
-          elem.innerText = value;
-          break;
-        case '$textReplace':
-          elem.innerText = elem.innerText.replace(...value);
-          break;
-        case '$html':
-          elem.innerHTML = value;
-          break;
-        case '$on':
-          elem.addEventListener(...value);
-          break;
-        case '$append':
-          elem.appendChild(value);
-          break;
-        case '$render':
-          const render = () => elem.innerHTML = value(handler.data);
-          handler.renders.push(render);
-          render();
-          break;
-      }
+      switchCase(elem, key, value);
     },
   };
 
